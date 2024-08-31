6f0aee7313be9f65ecbe44390f3fb032753c9989
Mateusz Pawluk <pawluk1988@icloud.com>
2024-08-31 14:32:02 +0200

    API

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-const btn1 = document.querySelector(".btn-1");
-const btn2 = document.querySelector(".btn-2");
-const btn3 = document.querySelector(".btn-3");
+const btn = document.querySelector("button");
+const img = document.querySelector("img");
 
-function Meals(name, price) {
-  this.name = name;
-  this.price = price;
-}
+const URL = "https://dog.ceo/api/breeds/image/random";
 
-const meal1 = new Meals("Schabowy", 32);
-const meal2 = new Meals("Pizz", 23);
-const meal3 = new Meals("Zupa", 18);
-
-Meals.prototype.msg = function () {
-  console.log(`${this.name} kosztuje ${this.price}zł`);
+const dogs = () => {
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => img.setAttribute("src", data.message))
+    .catch((err) => console.error(err));
 };
 
-btn1.addEventListener("click", () => {
-  meal1.msg();
-});
-btn2.addEventListener("click", () => {
-  meal2.msg();
-});
-btn3.addEventListener("click", () => {
-  meal3.msg();
-});
+btn.addEventListener("click", dogs);
